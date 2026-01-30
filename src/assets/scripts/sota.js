e6c206b256c32755117c0a1d2627f0e043f1bdd8
enoa153974 <aoi.153974@gmail.com>
2026-01-30 17:44:10 +0900

    デプロイエラー解消

--- src/assets/scripts/sota.js
+++ src/assets/scripts/sota.js
@@ -1,7 +1,7 @@
 import '../styles/style.scss';
 import { initSoChecklist } from './ui/sotaList.js';
 import { initSotaIllustPanel } from './ui/sotaIllustPanel.js';
-import { waitForImages } from './features/waitForImages.js';
+import { waitForImages } from './util.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     // HTML読み込みが終わったあとに実行される処理
