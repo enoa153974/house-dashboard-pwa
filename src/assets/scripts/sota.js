ed004af86970a8f8dc0966349d89da7ef0f816e6
enoa153974 <aoi.153974@gmail.com>
2026-01-30 18:28:38 +0900

    そたページロード修正

--- src/assets/scripts/sota.js
+++ src/assets/scripts/sota.js
@@ -2,12 +2,13 @@ import '../styles/style.scss';
 import { initSoChecklist } from './ui/sotaList.js';
 import { initSotaIllustPanel } from './ui/sotaIllustPanel.js';
 
+/* DOM構築が終わったら初期化 */
 window.addEventListener('DOMContentLoaded', () => {
-
     initSoChecklist();
     initSotaIllustPanel();
-    // HTML読み込みが終わったあとに実行される処理
-    window.addEventListener('load', () => {
-        document.querySelector('.sota-page')?.classList.add('is-ready');
-    });
+});
+
+/* 画像・フォント含めて全部読み終わったら表示 */
+window.addEventListener('load', () => {
+    document.querySelector('.sota-page')?.classList.add('is-ready');
 });
